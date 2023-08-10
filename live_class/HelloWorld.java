cb5716c3c77d589e2096c388b60355e1b8e6a648
berkaneris <berkan.eris.10@gmail.com>
2023-08-10 23:07:58 +0300

    testing of loading

---
+++ live_class/HelloWorld.java
@@ -0,0 +1,11 @@
+package week_01.live_class;
+
+public class HelloWorld {
+
+
+    public static void main (String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("");
+    }
+}
