45a9b91695aa82a3cff6a26eca14e788f49fa2fd
Berkan <141026290+berkaneris@users.noreply.github.com>
2023-08-12 16:50:21 +0300

    Delete Test.java

--- week_02/assigments/Test.java
+++
@@ -1,9 +0,0 @@
-package week_02.assigments;
-
-public class Test {
-    public static void main(String[] args) {
-        double deger=(long)((365 * 24 * 60 * 60 / 7.0) - (365 * 24 * 60 * 60 / 13.0) +
-                (365 * 24 * 60 * 60 / 45.0)) + 312032486;
-        System.out.println(deger);
-    }
-}
