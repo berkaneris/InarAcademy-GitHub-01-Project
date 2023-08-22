2b152fad6b5a1f978328ba183c72fea90bea11c5
berkaneris <berkan.eris.10@gmail.com>
2023-08-22 16:22:00 +0300

    The changes

--- week_03/assigments/Question_03_11.java
+++ week_03/assigments/Question_03_11.java
@@ -9,53 +9,33 @@ public class Question_03_11 {
         int year = input.nextInt();
         System.out.print("Enter a number of month: ");
         int month = input.nextInt();
-        if (year % 4 == 0 && month == 1) {
+        if (month == 1) {
             System.out.println("January " + year + " had 31 days.");
-        } else if (year % 4 == 0 && month == 2) {
-            System.out.println("February " + year + " had 29 days.");
-        } else if (year % 4 == 0 && month == 3) {
-            System.out.println("March " + year + " had 31 days.");
-        } else if (year % 4 == 0 && month == 4) {
-            System.out.println("April " + year + " had 30 days.");
-        } else if (year % 4 == 0 && month == 5) {
-            System.out.println("May " + year + " had 31 days.");
-        } else if (year % 4 == 0 && month == 6) {
-            System.out.println("June " + year + " had 30 days.");
-        } else if (year % 4 == 0 && month == 7) {
-            System.out.println("July " + year + " had 31 days.");
-        } else if (year % 4 == 0 && month == 8) {
-            System.out.println("August " + year + " had 31 days.");
-        } else if (year % 4 == 0 && month == 9) {
-            System.out.println("September " + year + " had 30 days.");
-        } else if (year % 4 == 0 && month == 10) {
-            System.out.println("October " + year + " had 31 days.");
-        } else if (year % 4 == 0 && month == 11) {
-            System.out.println("November " + year + " had 30 days.");
-        } else if (year % 4 == 0 && month == 12) {
-            System.out.println("December " + year + " had 31 days.");
-        } else if ( month == 1) {
-            System.out.println("January " + year + " had 31 days.");
-        } else if ( month == 2) {
+        } else if (month == 2 && year % 4 != 0 ) {
             System.out.println("February " + year + " had 28 days.");
-        } else if ( month == 3) {
+        } else if (month == 2 && year % 100 == 0 && year % 400 != 0) {
+            System.out.println("February " + year + " had 28 days.");
+        } else if (month == 2 ) {
+            System.out.println("February " + year + " had 29 days.");
+        } else if (month == 3) {
             System.out.println("March " + year + " had 31 days.");
-        } else if ( month == 4) {
+        } else if (month == 4) {
             System.out.println("April " + year + " had 30 days.");
-        } else if ( month == 5) {
+        } else if (month == 5) {
             System.out.println("May " + year + " had 31 days.");
-        } else if ( month == 6) {
+        } else if (month == 6) {
             System.out.println("June " + year + " had 30 days.");
-        } else if ( month == 7) {
+        } else if (month == 7) {
             System.out.println("July " + year + " had 31 days.");
-        } else if ( month == 8) {
+        } else if (month == 8) {
             System.out.println("August " + year + " had 31 days.");
-        } else if ( month == 9) {
+        } else if (month == 9) {
             System.out.println("September " + year + " had 30 days.");
-        } else if ( month == 10) {
+        } else if (month == 10) {
             System.out.println("October " + year + " had 31 days.");
-        } else if ( month == 11) {
+        } else if (month == 11) {
             System.out.println("November " + year + " had 30 days.");
-        } else if ( month == 12) {
+        } else if (month == 12) {
             System.out.println("December " + year + " had 31 days.");
         }
         else {
